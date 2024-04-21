5a102158baacaf4b06835c6ad5f60e9253d118c7
smartprincess <ayeshajawaid004@gmail.com>
2024-04-21 19:52:41 +0500

    first version

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+async function para() {
+    let user = await inquirer.prompt({
+        name: "userInput",
+        message: "Please Enter A Text To Count It's Characters",
+        type: "input"
+    });
+    let count = user.userInput.replace(/\s/g, '');
+    // let count = user.userInput.trim(); 
+    let text = count.length;
+    console.log(`THE NUMBER OF CHARACTERS IN YOUR TEXT IS ${text}`);
+}
+para();
